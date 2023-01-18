ab7f24b55e8403972bbe276d1dcbd3daf3e85c7d
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-01-18 01:32:37 -0600

    Set fish env vars regardless of login/interactive

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,3 @@
-set -Ux XDG_CONFIG_HOME $HOME/.config
-set -Ux XDG_CACHE_HOME $HOME/.cache
-set -Ux XDG_DATA_HOME $HOME/.local/share
-set -Ux XDG_STATE_HOME $HOME/.local/state
-
 fish_vi_key_bindings
 
 if test ! -d "$HOME/.dotfiles"
