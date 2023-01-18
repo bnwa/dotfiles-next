e1e4248b56d5e3ea2ee8d931be6fc74924b51b1b
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-01-18 02:03:35 -0600

    Set fish shell var

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,4 +1,12 @@
-set -Ux XDG_CONFIG_HOME $HOME/.config
-set -Ux XDG_CACHE_HOME $HOME/.cache
-set -Ux XDG_DATA_HOME $HOME/.local/share
-set -Ux XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
+
+if test -x (which nvim)
+  set -gx SHELL (which nvim)
+else if test -x (which vim)
+  set -gx SHELL (which vim)
+else
+  set -gx SHELL /usr/bin/vi
+end
