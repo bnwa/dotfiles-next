66a7b805413e0bd7052dc7b0ee1bd2212118b427
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-01-21 08:21:42 +0000

    Er, don't set /usr/bin/nvim to a vi-variant

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,11 +2,3 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_STATE_HOME $HOME/.local/state
-
-if test -x (which nvim)
-  set -gx SHELL (which nvim)
-else if test -x (which vim)
-  set -gx SHELL (which vim)
-else
-  set -gx SHELL /usr/bin/vi
-end
