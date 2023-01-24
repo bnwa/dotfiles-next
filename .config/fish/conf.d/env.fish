7f0c4efd75517ae024cae04fa2e706fde283dd3d
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-01-23 23:41:19 -0600

    Set EDITOR/LANG/SHELL in env.fish

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,3 +1,6 @@
+set -gx EDITOR /usr/local/bin/hx
+set -gx LANG en_US.UTF-8
+set -gx SHELL /usr/local/bin/fish
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
