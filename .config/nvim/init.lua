775a2dcd24b583641a256bb37c337f599908b321
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-04-01 21:55:23 -0500

    Add telescope-undo and accompanying map

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -150,13 +150,21 @@ local setup_telescope = function()
       },
       ['ui-select'] = {
         require('telescope.themes').get_dropdown {},
-      }
+      },
+      undo = {
+        side_by_side = true,
+        layout_strategy = 'vertical',
+        layout_config = {
+          preview_height = 0.8,
+        },
+      },
     },
   }
   scope.load_extension('file_browser')
   scope.load_extension('fzf')
   scope.load_extension('lazy')
   scope.load_extension('ui-select')
+  scope.load_extension('undo')
 end
 
 local function setup_treesitter()
@@ -264,11 +272,16 @@ require('lazy').setup {
 
 
   -- PICKER
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.1', config = setup_telescope, dependencies = { 'nvim-lua/plenary.nvim' } },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-  { 'nvim-telescope/telescope-file-browser.nvim', dependencies = { 'nvim-lua/plenary.nvim'  } },
-  { 'nvim-telescope/telescope-ui-select.nvim' },
-  { 'tsakirist/telescope-lazy.nvim' },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.1', config = setup_telescope,
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+      { 'nvim-telescope/telescope-file-browser.nvim', dependencies = { 'nvim-lua/plenary.nvim'  } },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'tsakirist/telescope-lazy.nvim' },
+      { 'debugloop/telescope-undo.nvim' },
+    }
+  },
 
   -- COLORSCHEMES
   { 'ramojus/mellifluous.nvim',  dependencies = { 'rktjmp/lush.nvim' }, priority = 1000, lazy = false  },
@@ -340,6 +353,12 @@ map('n', [[<leader>fl]], function()
     sort_mru = true,
   }
 end)
+
+map('n', [[<leader>fu]], function()
+  local picker = require 'telescope'
+  picker.extensions.undo.undo()
+end)
+
 
 -- EVENTS
 local function on(match, events, listener)
