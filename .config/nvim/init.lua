38142ee6de36554d4395bdc1c819f1c2ee3737c9
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-04-12 23:34:32 -0500

    Update maps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -172,12 +172,12 @@ local function setup_treesitter()
       'json',
       'lua',
       'markdown',
+      'rust',
       'scss',
       'sql',
       'toml',
       'typescript',
       'vim',
-      'vimdoc',
       'yaml',
     },
     highlight = {
@@ -351,12 +351,12 @@ map('n', { desc = "Find files under current working directory" }, [[<leader>ff]]
 end)
 
 if fn.system({'which', 'ripgrep' }) then
-  map('n', { desc = "Live grep if ripgrep installed" }, [[<leader>s]], function()
+  map('n', { desc = "Live grep if ripgrep installed" }, [[<leader>fg]], function()
     require('telescope.builtin').live_grep {}
   end)
 end
 
-map('n', { desc = "Find symbol in buffer via treesitter" }, [[<leader>y]], function()
+map('n', { desc = "Find symbol in buffer via treesitter" }, [[<leader>fs]], function()
   require('telescope.builtin').treesitter {}
 end)
 
@@ -382,7 +382,17 @@ map('n', { desc = "List registers and paste selected" }, [[<leader>f"]], functio
   picker.registers {}
 end)
 
+map('n', { desc = "Show type information in floating window relative to cursor" }, [[<leader>d]], function()
+  vim.lsp.buf.hover()
+end)
 
+map('n', { desc = "Rename symbol under cursor" }, [[<leader>sr]], function()
+  vim.lsp.buf.rename()
+end)
+
+map('n', { desc = "Select from listed code actions", }, [[<leader>sa]], function()
+  vim.lsp.buf.code_action()
+end)
 -- EVENTS
 local function on(match, events, listener)
   autocmd(events, {
