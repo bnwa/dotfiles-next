a254571cd260242ab5477bd1e11dee75669be5ad
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-04-22 18:59:01 -0500

    Ensure homebrew configured entering fish shell

---
+++ .config/fish/conf.d/homebrew.fish
@@ -0,0 +1,5 @@
+if test -d /opt/homebrew/bin; and test -x /opt/homebrew/bin/brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else if -d /usr/local/bin; and test -x /usr/local/bin/brew
+    eval "$(/usr/local/bin/brew shellenv)"
+end
