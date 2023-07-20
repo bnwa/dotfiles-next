3044403c34fa8d284aed8e77f970e28f6d381961
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-07-20 08:35:38 -0500

    Fix Fish java config when no JDK present

--- .config/fish/conf.d/java.fish
+++ .config/fish/conf.d/java.fish
@@ -1,6 +1,6 @@
 if test -r (pwd)/.java-version
     set -l java_version (cat .java-version)
     set -x JAVA_HOME (/usr/libexec/java_home -v $java_version)
-else
+else if test (/usr/libexec/java_home -V 2&> /dev/null)
     set -x JAVA_HOME (/usr/libexec/java_home -v 17)
 end
