df8d9b1183f7d4e277cfc29aa964594cc521119f
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2023-09-03 15:05:07 -0500

    Update Telescope to 0.1.x branch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -489,7 +489,7 @@ require('lazy').setup {
 
 
   -- PICKER
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.1', config = setup_telescope,
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', config = setup_telescope,
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
