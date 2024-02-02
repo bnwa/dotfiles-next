4fcfbd6ad1a90fdd2deba1470e506f9896e1362a
Benoît Barberousse <heybenoit+github@gmail.com>
2024-02-02 15:25:33 -0600

    Diagnostics should be toggleable report on current buf only

--- .config/nvim/lua/user/plugins/which_key.lua
+++ .config/nvim/lua/user/plugins/which_key.lua
@@ -47,7 +47,10 @@ return {
         },
         e = {
           function()
-            vim.diagnostic.setqflist()
+            local buf_issues = vim.diagnostic.get(0)
+            local qf_issues = vim.diagnostic.toqflist(buf_issues)
+            vim.fn.setqflist(qf_issues, 'r')
+            vim.cmd 'cw'
           end,
           "Show LSP diagnostics in quickfix list"
         },
