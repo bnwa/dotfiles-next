3e7f6a8d0dfd364a9d1bc02dd8a6383582d4eb6a
Benoît Barberousse <heybenoit+github@gmail.com>
2024-03-20 18:51:31 -0500

    Update dark mode schedule in nvim

--- .config/nvim/lua/user/utils/time.lua
+++ .config/nvim/lua/user/utils/time.lua
@@ -4,7 +4,7 @@ function M.is_night()
   local date_str = os.date()
   local date_parts = vim.split(date_str, '[%s%p]+')
   local date_hour = vim.fn.str2nr(date_parts[4])
-  local is_night = date_hour >= 16 or date_hour < 7
+  local is_night = date_hour >= 19 or date_hour < 7
 
   return is_night
 end
