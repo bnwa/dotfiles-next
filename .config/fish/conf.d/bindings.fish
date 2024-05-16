f020712bc38b4a4fe4931bd254f589d58882e541
Benoît Barberousse <heybenoit+github@gmail.com>
2024-05-16 18:59:32 -0500

    Fix <C-f> bind conflict between fish/zellij

--- .config/fish/conf.d/bindings.fish
+++ .config/fish/conf.d/bindings.fish
@@ -1,7 +1,7 @@
 fish_vi_key_bindings
 
 for mode in insert default visual
-  bind -M $mode \cf forward-char
+  bind -M $mode \cg forward-char
   # Ctrl+Shift+f for incremental accept
   bind -M $mode \ct forward-word
 end
