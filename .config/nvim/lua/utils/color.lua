82d08792ece2d047b7c799bce12cc81c96b50696
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-07-13 09:45:34 -0500

    Add types to color.lua

--- .config/nvim/lua/utils/color.lua
+++ .config/nvim/lua/utils/color.lua
@@ -3,6 +3,9 @@ local opt = vim.opt
 
 local M = {}
 
+---Sets 'background' option to 'light' or 'dark' given min and max wall clock thresholds in 24h format
+---@param min integer[] # A list of two integers, first specifying hour in 24h format, second specifying minutes
+---@param max integer[] # A list of two integers, first specifying hour in 24h format, second specifying minutes
 function M.light_or_dark_mode(min, max)
   if time.between(min, max) then
     opt.background = "dark"
