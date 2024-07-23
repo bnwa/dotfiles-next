30d314998255c048e7a8f9be99d3212d87ed13f2
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-07-22 22:03:47 -0500

    Replace treesitter-context with barbercue

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -314,4 +314,19 @@ return {
       require("lspkind").init(opts)
     end,
   },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      exclude_filetypes = {
+        'netrw',
+        'toggleterm',
+      }
+    },
+  },
 }
