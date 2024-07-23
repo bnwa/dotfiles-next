3bad6124ca23fc5693bbdecfd3c45873c7cb3b88
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-07-23 10:40:11 -0500

    Add oil.nvim for file browsing

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -329,4 +329,13 @@ return {
       }
     },
   },
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      delete_to_trash = true,
+    },
+    -- Optional dependencies
+    --dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
 }
