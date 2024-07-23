9dd7bcfdccc69f30f5126cd683b573a1c4c69c11
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-07-22 21:34:51 -0500

    Configure filename display in lualine

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -154,7 +154,30 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    opts = {
+      options = {
+        component_separators = { left = '', right = '' },
+        globalstatus = true,
+        ignore_focus = {
+          'netrw',
+        },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+            'filename',
+            newfile_status = true, -- Display 'new file status
+            path = 1, -- relative path
+          },
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+    },
   },
   { 'tpope/vim-fugitive' },
   {
