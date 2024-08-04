50afc9a54ac77c229a36a32aecab674647d1d69b
Benoît Barberousse <heybenoit+github@gmail.com>
2024-08-04 00:37:45 -0500

    Document JDTLS extended capabilities

--- .config/nvim/lua/user/plugins/lang/java.lua
+++ .config/nvim/lua/user/plugins/lang/java.lua
@@ -31,6 +31,37 @@ return {
       servers = {
         jdtls = {
           filetypes = ft,
+--[[ Handled by nvim-java
+          init_options = {
+            extended_capabilities = {
+              actionableNotificationSupported = false, -- default: false
+              actionableRuntimeNotificationSupport = false, -- default: false
+              advancedExtractRefactoringSupport = false, -- default: false
+              advancedGenerateAccessorsSupport = false, -- default: false
+              advancedIntroduceParameterRefactoringSupport = false, -- default: false
+              advancedOrganizeImportsSupport = false, -- default: false
+              advancedUpgradeGradleSupport = false, -- default: false
+              classFileContentsSupport = false, -- default: false
+              clientDocumentSymbolProvider = false, -- default: false
+              clientHoverProvider = false, -- default: false
+              excludedMarkerTypes = {}, -- default: []
+              extractInterfaceSupport = false, -- default: false
+              generateConstructorsPromptSupport = false, -- default: false
+              generateDelegateMethodsPromptSupport = false, -- default: false
+              generateToStringPromptSupport = false, -- default: false
+              gradleChecksumWrapperPromptSupport = false,-- default: false
+              hashCodeEqualsPromptSupport = false, -- default: false
+              inferSelectionSupport = false,-- default: false
+              moveRefactoringSupport = false,-- default: false
+              onCompletionItemSelectedCommand = false,-- default: false
+              overrideMethodsPromptSupport = false,-- default: false
+              progressReportProvider = false, -- default: false
+              shouldLanguageServerExitOnShutdown = false,-- default: false
+              skipProjectConfiguration = false, -- default: false
+              skipTextEventPropagation = false, -- default: false
+            }
+          },
+--]]
           on_attach = function(client, _)
             client.notify('workspace/didChangeConfiguration', {
               settings = settings
