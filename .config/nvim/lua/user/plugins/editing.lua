1e69494265f7085cab5f56f6d576b321824ae042
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-08-20 12:25:42 -0500

    Disable f/F/t/T in flash.nvim

--- .config/nvim/lua/user/plugins/editing.lua
+++ .config/nvim/lua/user/plugins/editing.lua
@@ -81,7 +81,11 @@ return {
   },
   {
     "folke/flash.nvim",
-    opts = {},
+    opts = {
+      modes = {
+        char = { enabled = false }
+      }
+    },
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
