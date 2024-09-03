96fc670c12d1c0d931fdc5599731ab822de6c1c5
Benoît Barberousse <heybenoit+github@gmail.com>
2024-09-03 11:34:52 -0500

    Fix: copilot should use default node bin

--- .config/nvim/lua/user/plugins/copilot.lua
+++ .config/nvim/lua/user/plugins/copilot.lua
@@ -1,18 +1,6 @@
 local path = require 'user.utils.path'
 
-local brew_node = '/opt/Homebrew/bin/node'
-
 return {
-  {
-    'zbirenbaum/copilot.lua',
-    enabled = function() return path.can_exec(brew_node) end,
-    event = 'InsertEnter',
-    opts = {
-      copilot_node_command = brew_node,
-      panel = { enabled = false },
-      suggestion = { enabled = false },
-    }
-  },
   {
     "zbirenbaum/copilot-cmp",
     opts = {
@@ -21,5 +9,13 @@ return {
     config = function (_, opts)
       require("copilot_cmp").setup(opts)
     end
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    opts = {
+      panel = { enabled = false },
+      suggestion = { enabled = false },
+    }
   },
 }
