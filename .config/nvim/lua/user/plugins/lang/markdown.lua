aace727e72cdc1d3685b5916ea54c62aeb2f2450
Benoît Barberousse <heybenoit+github@gmail.com>
2024-09-09 14:43:49 -0500

    Add markdown config

---
+++ .config/nvim/lua/user/plugins/lang/markdown.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.icons',
+    },
+  },
+}
