495e7feaa420d322cadba4506959a11cfdeee3ef
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-09-26 11:25:26 -0500

    Improve config cmd for edits,listings

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,7 +1,30 @@
 function config
-	if test -d $HOME/.dotfiles
-		git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
-	else
-		echo "Directory not found: ~/.dotfiles -- Clone repo before using config command"
-	end
+  set -l cmd $argv[1]
+
+  if not test -d $HOME/.dotfiles
+	  echo "Directory not found: ~/.dotfiles -- Clone repo before using config command"
+    return 1
+  end
+
+  switch $cmd
+    case ls
+      git --git-dir=$HOME/.dotfiles --work-tree=$HOME ls-files $argv[2..-1]
+    case e
+      set -l target $argv[2]
+      switch $target
+        case nvim
+          nvim --cmd 'cd ~/.config/nvim' ~/.config/nvim/{*,**/*}.lua
+        case wez
+          nvim --cmd 'cd ~/.config/wezterm' ~/.config/wezterm/{*,**/*}.lua
+        case fish
+          nvim --cmd 'cd ~/.config/fish' ~/.config/fish/{*,**/*}.fish
+        case zel
+          nvim --cmd 'cd ~/.config/zellij' ~/.config/zellij/{*,**/*}.kdl
+        case '*'
+          echo "Unknown argument to 'e' encountered:" $target
+          return 1
+      end
+    case '*'
+		  git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
+  end
 end
