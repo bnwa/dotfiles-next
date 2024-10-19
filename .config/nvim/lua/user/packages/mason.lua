02511f84c1846751673793a001c87d54f035f9d0
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-10-19 13:22:39 -0500

    Enhance mason package management

--- .config/nvim/lua/user/packages/mason.lua
+++ .config/nvim/lua/user/packages/mason.lua
@@ -6,4 +6,17 @@ require('mason').setup {
 }
 
 -- Callback func required for async execution
-require('mason-registry').refresh(function()end)
+require('mason-registry').refresh(function()
+    require('mason-tool-installer').setup {
+        ensure_installed = {
+            'vtsls',
+        },
+        integrations = {
+            ['mason-lspconfig'] = false,
+        },
+        run_on_start = false,
+    }
+    vim.schedule(function()
+        vim.cmd 'MasonToolsUpdate'
+    end)
+end)
