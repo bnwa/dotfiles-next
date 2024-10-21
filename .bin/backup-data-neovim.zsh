6c5e2ff504a9d767e4ddea2ab7726cd6ca2688e3
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-10-21 10:35:31 -0500

    Add neovim scripts to .bin

---
+++ .bin/backup-data-neovim.zsh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+
+mv $HOME/.local/share/nvim $HOME/.local/share/nvim-bak
+mv $HOME/.local/state/nvim $HOME/.local/state/nvim-bak
+mv $HOME/.cache/nvim $HOME/.cache/nvim-bak
---
+++ .bin/restore-data-neovim.zsh
@@ -0,0 +1,6 @@
+#! /bin/zsh
+
+mv $HOME/.local/share/nvim-bak $HOME/.local/share/nvim
+mv $HOME/.local/state/nvim-bak $HOME/.local/state/nvim
+mv $HOME/.cache/nvim-bak $HOME/.cache/nvim
+
