fa019071a59f0cc80d56c51cd8ba57bbcf39f0ce
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-10-31 19:57:38 -0500

    Start fish-lsp correctly

--- .config/nvim/lua/user/plugins/lang/fish.lua
+++ .config/nvim/lua/user/plugins/lang/fish.lua
@@ -8,7 +8,7 @@ return {
     servers = {
      fish_lsp = {
        default_config = {
-         cmd = { 'fish-language-server', 'start' },
+         cmd = { 'fish-language-server' },
          filetypes = { 'fish' },
          root_dir = function()
            return vim.fs.root(0, { 'config.fish' })
