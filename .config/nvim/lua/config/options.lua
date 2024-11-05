33b5bed34fd5579c45da56b49fd9976e4b221eaa
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-11-05 16:18:29 -0600

    Disable format on write

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@ local cmd = vim.cmd
 local opt = vim.opt
 opt.listchars.extends = "»" -- Show this char when line excedes window right
 opt.listchars.precedes = "«" -- Show this char when line excedes window left
+vim.g.autoformat = false
 if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_theme = "auto"
