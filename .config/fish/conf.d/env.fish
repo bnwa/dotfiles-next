4fbed4d3b531c7b14ccafb931a522fd5f27f742c
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-11-06 13:23:02 -0600

    Remove legacy Homebrew check

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -4,7 +4,6 @@ set -gx HOMEBREW_NO_AUTO_UPDATE 1
 set -gx HOMEBREW_NO_INSTALL_UPGRADE 1
 set -gx LANG en_US.UTF-8
 test -x /opt/homebrew/bin/fish && set -gx SHELL /opt/homebrew/bin/fish
-test -x /usr/local/bin/fish && set -gx SHELL /usr/local/bin/fish
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
