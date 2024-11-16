67f1a98a49586f2eb9ac6a209354eac5f7460613
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-11-16 11:55:19 -0600

    Disable bufferline, abbrev branch in lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,6 @@
 --- @type LazySpec[]
 return {
+  { 'akinsho/bufferline.nvim', enabled = false },
   {
     "akinsho/toggleterm.nvim",
     version = "*",
@@ -85,4 +86,21 @@ return {
       },
     },
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      sections = {
+        lualine_b = {
+          { 'branch',
+            --- @param str string
+            --- @return string
+            fmt = function(str)
+              if str:len() > 16 then return str:sub(1, 16)
+              else return str end
+            end
+          }
+        }
+      }
+    }
+  }
 }
