de722a057fe82475a073d1478da6a1708bbf308c
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2024-11-16 12:24:31 -0600

    Update LazyVim, add oil.nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,4 @@
+--- @type LazySpec[]
 return {
   {
     "tzachar/highlight-undo.nvim",
@@ -20,4 +21,17 @@ return {
       require("virt-column").setup({ char = "▕" })
     end,
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    enabled = false
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  }
 }
