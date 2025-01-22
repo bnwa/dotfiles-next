c2cfb10e4bc3b334f0af9dcac4cc30528f16c1f0
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2025-01-22 12:43:41 -0600

    Disable blink.cmp in copilot-chat, snacks.nvim picker

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,3 +1,17 @@
+---@type string[]
+local disabled_filetypes = {
+  "copilot-chat",
+  "snacks_picker_input",
+}
+
 --- @type LazySpec[]
 return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      enabled = function()
+        return not vim.tbl_contains(disabled_filetypes, vim.bo.filetype)
+      end
+    }
+  }
 }
