c0a88cc34659b8650b59a660e38183d2e8578596
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2025-01-22 12:42:47 -0600

    Update LazyVim packages/extras

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -30,63 +30,6 @@ return {
     -- stylua: ignore end
   },
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "debugloop/telescope-undo.nvim",
-        config = function()
-          LazyVim.on_load("telescope", function()
-            local ok, err = pcall(require("telescope").load_extension, "undo")
-            if not ok then
-              LazyVim.warn("Failed to load telescope-undo: " .. err)
-            end
-          end)
-        end,
-      },
-    },
-    opts = {
-      defaults = {
-        dynamic_preview_title = true,
-        layout_config = {
-          preview_cutoff = 20,
-        },
-        path_display = { "truncate" },
-        preview = {
-          filetype_hook = function(_, buf, opts)
-            if opts.ft == "bigfile" then
-              local preview = require("telescope.previewers.utils")
-              local msg = "File too large, enable previewing with `bigfile` filetype"
-              preview.set_preview_message(buf, opts.winid, msg)
-              return false
-            else
-              return true
-            end
-          end,
-          treesitter = true,
-        },
-        wrap_results = true,
-      },
-      extensions = {
-        undo = {
-          side_by_side = true,
-          layout_strategy = "vertical",
-          layout_config = {
-            preview_height = 0.8,
-          },
-        },
-      },
-    },
-    keys = {
-      {
-        "<leader>fu",
-        function()
-          require("telescope").picker.extensions.undo.undo()
-        end,
-        { desc = "View undo tree and apply changes" },
-      },
-    },
-  },
-  {
     'nvim-lualine/lualine.nvim',
     opts = {
       sections = {
@@ -102,5 +45,5 @@ return {
         }
       }
     }
-  }
+  },
 }
