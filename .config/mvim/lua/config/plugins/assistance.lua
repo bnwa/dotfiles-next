bbeeed4726e3e116975c6de36b98fe29962358a2
Benoit Barberousse <74591246+bnwa@users.noreply.github.com>
2025-08-13 15:34:33 -0500

    mvim: add MCPHub.nvim for agent tooling

--- .config/mvim/lua/config/plugins/assistance.lua
+++ .config/mvim/lua/config/plugins/assistance.lua
@@ -15,7 +15,25 @@ return {
       ---@type avante.Config
       return {
         auto_suggestions_provider = provider,
-        disabled_tools = { 'python' },
+        -- Using function prevents requiring mcphub before it's loaded
+        custom_tools = function()
+          return {
+            require("mcphub.extensions.avante").mcp_tool(),
+          }
+        end,
+        disabled_tools = {
+          'python',        -- Claude goes nuts w/ Python
+          "list_files",    -- MCPHub provides the tools from here
+          "search_files",
+          "read_file",
+          "create_file",
+          "rename_file",
+          "delete_file",
+          "create_dir",
+          "rename_dir",
+          "delete_dir",
+          "bash",
+        },
         file_selector = {
           file_selector = 'snacks'
         },
@@ -55,6 +73,12 @@ return {
         selector = {
           provider = 'snacks',
         },
+        -- system_prompt as function ensures LLM always has latest MCP server state
+        -- This is evaluated for every message, even in existing chats
+        system_prompt = function()
+          local hub = require("mcphub").get_hub_instance()
+          return hub and hub:get_active_servers_prompt() or ""
+        end,
         web_search_engine = {
           provider = 'google'
         },
@@ -100,6 +124,23 @@ return {
           }
         },
       },
+      {
+        "ravitemer/mcphub.nvim",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+        },
+        build = "bundled_build.lua",  -- Bundles `mcp-hub` binary along with the neovim plugin
+        config = function()
+          require("mcphub").setup({
+            extensions = {
+              avante = {
+                make_slash_commands = true, -- make /slash commands from MCP server prompts
+              }
+            },
+            use_bundled_binary = true,  -- Use local `mcp-hub` binary
+          })
+        end,
+      }
     },
   },
 }
