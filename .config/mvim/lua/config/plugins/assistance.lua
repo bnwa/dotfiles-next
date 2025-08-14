5f531535c02b915845d915e9c3cbf95fdeef4e47
Benoit Barberousse <74591246+bnwa@users.noreply.github.com>
2025-08-13 23:16:47 -0500

    Revert "mvim: add MCPHub.nvim for agent tooling"
    This reverts commit bbeeed4726e3e116975c6de36b98fe29962358a2.

--- .config/mvim/lua/config/plugins/assistance.lua
+++ .config/mvim/lua/config/plugins/assistance.lua
@@ -15,25 +15,7 @@ return {
       ---@type avante.Config
       return {
         auto_suggestions_provider = provider,
-        -- Using function prevents requiring mcphub before it's loaded
-        custom_tools = function()
-          return {
-            require("mcphub.extensions.avante").mcp_tool(),
-          }
-        end,
-        disabled_tools = {
-          'python',        -- Claude goes nuts w/ Python
-          "list_files",    -- MCPHub provides the tools from here
-          "search_files",
-          "read_file",
-          "create_file",
-          "rename_file",
-          "delete_file",
-          "create_dir",
-          "rename_dir",
-          "delete_dir",
-          "bash",
-        },
+        disabled_tools = { 'python' },
         file_selector = {
           file_selector = 'snacks'
         },
@@ -73,12 +55,6 @@ return {
         selector = {
           provider = 'snacks',
         },
-        -- system_prompt as function ensures LLM always has latest MCP server state
-        -- This is evaluated for every message, even in existing chats
-        system_prompt = function()
-          local hub = require("mcphub").get_hub_instance()
-          return hub and hub:get_active_servers_prompt() or ""
-        end,
         web_search_engine = {
           provider = 'google'
         },
@@ -124,23 +100,6 @@ return {
           }
         },
       },
-      {
-        "ravitemer/mcphub.nvim",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-        },
-        build = "bundled_build.lua",  -- Bundles `mcp-hub` binary along with the neovim plugin
-        config = function()
-          require("mcphub").setup({
-            extensions = {
-              avante = {
-                make_slash_commands = true, -- make /slash commands from MCP server prompts
-              }
-            },
-            use_bundled_binary = true,  -- Use local `mcp-hub` binary
-          })
-        end,
-      }
     },
   },
 }
