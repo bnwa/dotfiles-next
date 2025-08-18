db9f50d12d9f7c7665713d2ea35bb607033d7d3e
Benoit Barberousse <74591246+bnwa@users.noreply.github.com>
2025-08-17 21:45:47 -0500

    fish: explicitly set XCode CLI tools' include paths

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -8,3 +8,6 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_STATE_HOME $HOME/.local/state
+## Mason fucks up includes for native Python C extensions
+test (xcode-select -p) && set -gx C_INCLUDE_PATH (xcode-select -p)/SDKs/MacOSX.sdk/usr/include/c++/v1
+test (xcode-select -p) && set -gx CPLUS_INCLUDE_PATH (xcode-select -p)/SDKs/MacOSX.sdk/usr/include/c++/v1
